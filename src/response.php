502e1fc555def5505854171524f631f6637a5a93
Salvatore Skare <skare.salvato@uwlax.edu>
2018-09-28 09:26:57 -0500

    Added in success() and error() shorthand functions

--- src/response.php
+++ src/response.php
@@ -5,19 +5,9 @@
  */
 
 /*
- * A generic success message
- */
-function success(): void {
-    $output = new HTTPResponse();
-    $payload->message = "Success";
-    $output->setPayload($payload);
-    $output->complete();
-}
-
-/*
  * A custom success message
  */
-function success($message): void {
+function success($message="Success"): void {
     $output = new HTTPResponse();
     $payload->message = $message;
     $output->setPayload($payload);
