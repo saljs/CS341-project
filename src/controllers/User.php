4445f9b239031ca25a354d3d1b008d2b643be433
Salvatore Skare <skare.salvato@uwlax.edu>
2018-10-01 02:20:33 -0500

    Added more to the SiteUser class

--- src/controllers/User.php
+++ src/controllers/User.php
@@ -33,7 +33,11 @@ class User {
                 error("Operation requires admin privileges");
                 return;
             }
-            //TODO: verify the key!
+            $admin = new SiteUser($args['email']);
+            if(!$admin->checkAuth($args['auth'])) {
+                error("Invalid token");
+                return;
+            }
         }
         
         //inserts the user into the database
@@ -56,19 +60,77 @@ class User {
 
 }
 
+/*
+ * Site user object
+ */
 class SiteUser {
 
+    public $id;
     public $email;
-    public $password;
+    private $hash;
     public $type;
     public $name;
     
-    function __construct($id) {
-        $this->email = $id;
+    /*
+     * Constructor
+     * @param email: The user's email address
+     */
+    function __construct($email) {
+        $this->email = $email;
+        $db = $GLOBALS['database'];
+        $result = $db->query("SELECT * from users WHERE email = '" . $this->email . "';");
+        $row = mysqli_fetch_assoc($result);
+        $this->hash = $row['password'];
+        $this->type = $row['type'];
+        $this->name = $row['name'];
+        $this->id =  $row['id'];
     }
 
-    function load(): int {
+    /*
+     * Authenticates the user
+     * @param password: The user's plaintext password
+     * @return An authentication token that is valid for 6 hours
+     */
+    function auth($password): string {
+        if(!password_verify($password, $this->hash)) {
+            throw new Exception("Invalid credentials");
+        }
         $db = $GLOBALS['database'];
+        $result = $db->query("SELECT * from logins WHERE user = '" . $this->id . "';");
+        if(mysqli_num_rows($result) > 0) {
+            //Previous login for this user
+            $row = mysqli_fetch_assoc($result);
+            if(time() - strtotime($row['created']) < 21600) { //6 hours until token expires
+                return $row['token'];
+            }
+            else {
+                if(!$db->query("DELETE FROM logins WHERE user = '" . $this->id . "';")) {
+                    throw new Exception($db->error);
+                }
+            }
+        }
+        //generate a new token
+        $token = bin2hex(random_bytes(128));
+        if(!$db->query("INSERT INTO logins (user, token) VALUES('" . $this->id . "', '" . $token . "');")) {
+            throw new Exception($db->error);
+        }
+        return $token;
+    }
+
+    /*
+     * Checks if the user is authenticated
+     * @param token: An authentication token
+     * @return True if authenticated, false otherwise
+     */
+    function checkAuth($token): bool {
+        $result = $db->query("SELECT * from logins WHERE user = '" . $this->id . "';");
+        if(mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            if(time() - strtotime($row['created']) < 21600 && $row['token'] == $token) { //6 hours until token expires
+                return true;
+            }
+        }
+        return false;
     }
 }
         
