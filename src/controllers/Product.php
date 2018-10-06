12a36b06497cb5bfc87c49f722617467f88ec1f0
JDCollins2020 <39606010+JDCollins2020@users.noreply.github.com>
2018-10-06 17:46:26 -0500

    Update Product.php

--- src/controllers/Product.php
+++ src/controllers/Product.php
@@ -89,7 +89,7 @@ class ViewableProduct{
         echo "<h1>unimplimented, creates user veiwable calling on product.html</h1>";
         echo $this->image;
         echo "<img src= '".$this->image."'>";
-        echo "<img src= 'example/img/cart.jpg'/>";
+        echo "<img src= 'CS341-project/example/img/cart.jpg'/>";
     }
 }
 ?>
