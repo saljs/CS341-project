0a847f226769ebb8b82228eb65bed52c1d419216
JDCollins2020 <39606010+JDCollins2020@users.noreply.github.com>
2018-10-06 16:58:41 -0500

    Update Product.php

--- src/controllers/Product.php
+++ src/controllers/Product.php
@@ -52,7 +52,7 @@ class ViewableProduct{
         echo $q;
          $result = $db->query($q); //fetch product by name from the db
         //checks if product with @param name exists
-        echo "<br> db query returned" .mysqli_num_rows($result). "results";
+        echo "<br> db query returned " .mysqli_num_rows($result). " results";
         //if(mysqli_num_rows($result) < 0) {
           //      error("product does not exist");//dne, return error
           //  echo "FAIL";
