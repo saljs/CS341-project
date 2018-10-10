18a8e703b171571646b4e3ad33be8fbfaf432c30
JDCollins2020 <39606010+JDCollins2020@users.noreply.github.com>
2018-10-10 16:42:44 -0500

    Update site.js

--- src/example/js/site.js
+++ src/example/js/site.js
@@ -89,7 +89,7 @@ function loadAllItems() {
 
 function itemList(items) {
     items.forEach(function(item) {
-        $('#products').append('<li><a href="' + baseURL + '/item.html?id=' + item.id + '">' 
+        $('#products').append('<li><a href="Product/get?id=' + item.id + '">' 
             + '<img src="' + item.image + '" class="productImg" width="400" height ="400"/>'
             + item.name 
             + '</a></li>');
