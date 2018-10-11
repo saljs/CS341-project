7708ba8463ff3131b0bef5f473329c2490ee594a
Bobakanoosh <jack.bobakanoosh@gmail.com>
2018-10-11 13:02:39 -0500

    Debug #8

--- src/controllers/Promotion.php
+++ src/controllers/Promotion.php
@@ -265,10 +265,11 @@ class ViewableDiscount {
 
             $row = mysqli_fetch_row($result);
 
-            echo "[ROW]: " . $row;
-            echo "[Row] Name: " . $row['name'] . "\n\n\n";
-            echo "[Row] Percent: " . $row['Percent'] . "\n\n\n";
-            echo "[Row] Enddate: " . $row['Enddate'] . "\n\n\n";
+            foreach($row as $key => $val) {
+
+                echo "Row Data: " . $key . "   " . $val . "\n\n\n";
+
+            }
 
 
             $this->code = $code;
