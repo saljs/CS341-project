c0f6688fa08a0e727b9dd56d6c76a0a465fa0674
Bobakanoosh <jack.bobakanoosh@gmail.com>
2018-10-24 14:48:17 -0500

    submission debug

--- src/example/js/site.js
+++ src/example/js/site.js
@@ -61,6 +61,7 @@ function createCategory(e) {
     e.preventDefault();
     var fields = $('#createCategory').serialize();
     fields += "&token=" + $.cookie('token');
+    console.log(fields);
     $.post('https://cs341group4.tk/Category/Create', fields)
     .done(function(data) {
         $('#message').html(data.message);
