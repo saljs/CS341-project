33ae4c20c8e6c0b8eca278cfcc3b6fcc8924acdc
Salvatore Skare <skare.salvato@uwlax.edu>
2018-10-29 11:30:19 -0500

    Hooked up add to cart button

--- src/example/js/site.js
+++ src/example/js/site.js
@@ -178,7 +178,7 @@ function loadCart() {
     }
 }
 
-function itemList(items) {
+function cartList(items) {
     items.forEach(function(item) {
         $('#cart').append('<li><a href="https://cs341group4.tk/' + baseURL +'/item.html?id=' + item.id + '">' 
             + '<img src="' + item.image + '" class="productImg" width="400" height ="400"/>'
