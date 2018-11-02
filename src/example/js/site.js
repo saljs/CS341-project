534f90dbd67bc72bf009bc8f0b3c158043ef714f
Bobakanoosh <jack.bobakanoosh@gmail.com>
2018-11-02 12:49:57 -0500

    fixing row width by changing preview image

--- src/example/js/site.js
+++ src/example/js/site.js
@@ -131,7 +131,7 @@ function itemList(items) {
         //    + item.name 
         //    + '</a></li>');
         $('#products').append('<div class="col-lg-4 col-md-6 mb-4">'+'<div class="card h-100">'+
-                '<img class="card-img-top" src="http://placehold.it/500x325" alt="">'+
+                '<img class="card-img-top" src="http://placehold.it/700x400" alt="">'+
                 '<div class="card-body">'+
                     '<h4 class="card-title">'+item.name+'</h4>'+
                     '<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>'+
