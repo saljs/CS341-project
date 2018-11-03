599e935de934423a38848e1d75c120bab82c8d5d
JDCollins2020 <39606010+JDCollins2020@users.noreply.github.com>
2018-11-03 18:07:42 -0500

    Update site.js
    card loaded in store.html now displays the correct item image

--- src/example/js/site.js
+++ src/example/js/site.js
@@ -167,7 +167,7 @@ function itemList(items) {
         //    + '</a></li>');
         $('#products').append('<div class="col-lg-4 col-md-6 mb-4">'+'' +
                                 '<div class="card h-100">'+
-                                    '<img class="card-img-top" src="http://placehold.it/700x400" alt="">'+
+                                    '<img class="card-img-top" src="'+item.image+'" alt="">'+
                                         '<div class="card-body">'+
                                             '<h4 class="card-title">'+item.name+'</h4>'+
                                             '<p class="card-text">'+item.description+'</p>'+
