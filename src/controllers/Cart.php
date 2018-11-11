421e16415082f73e145f954c96d90b71d3076769
JDCollins2020 <39606010+JDCollins2020@users.noreply.github.com>
2018-11-11 14:21:11 -0600

    Update Cart.php

--- src/controllers/Cart.php
+++ src/controllers/Cart.php
@@ -25,7 +25,7 @@ function getCartPrice($userId, $promotionCode):float {
     if($promotionCode) {
         //TODO: process promotions
        $promocode = $db->query("SELECT * FROM promotions WHERE code = '" . $promotionCode . "';");
-        $cost = $promocode['percent'];
+        $cost = (float)$promocode['percent'];
     }
     return $cost;
 }  
